d346011f0149fc3ab3c0cc6f8453c0e32d1e67b7
Gustavo <gustavomontiselli@gmail.com>
2024-08-28 15:40:02 -0300

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
